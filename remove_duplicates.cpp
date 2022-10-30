173a6759c3a20917060f9e7a64132f2782bf7ca3
constf <k.faraonov@normais.ru>
2022-10-30 22:04:42 +0300

    Sprint 6, RemoveDuplicates comments cleared.

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -1,15 +1,29 @@
 #include "remove_duplicates.h"
 #include <vector>
 #include <iterator>
-#include <algorithm>
 #include <iostream>
+
 #include "search_server.h"
 
 void RemoveDuplicates(SearchServer& search_server){
     std::vector<int> docs_to_remove;
+    std::set<std::set<std::string>> word_collection_checker;
 
-    for(auto iter_small = search_server.begin(); iter_small != search_server.end(); ++iter_small){
-        if (std::count(docs_to_remove.begin(), docs_to_remove.end(),*iter_small) > 0) continue;
+    for(const int doc_id : search_server){
+        const std::map<std::string, double>& words_map = search_server.GetWordFrequencies(doc_id);
+        std::set<std::string> words_set;
+        for(const auto val_pair : words_map) words_set.insert(val_pair.first);
+
+        if (word_collection_checker.count(words_set) > 0){
+            docs_to_remove.push_back(doc_id);
+        } else {
+            word_collection_checker.insert(words_set);
+        }
+
+        delete &words_map;
+
+
+        /*if (std::count(docs_to_remove.begin(), docs_to_remove.end(),*iter_small) > 0) continue;
 
         for(auto iter_big = next(iter_small); iter_big != search_server.end(); ++iter_big){
             if (std::count(docs_to_remove.begin(), docs_to_remove.end(),*iter_big) > 0) continue;
@@ -30,10 +44,9 @@ void RemoveDuplicates(SearchServer& search_server){
             }
             delete &small_set;
             delete &big_set;
-        }
+        }*/
     }
 
-    std::sort(docs_to_remove.begin(), docs_to_remove.end());
     for(int doc_id : docs_to_remove){
         using namespace std;
 
